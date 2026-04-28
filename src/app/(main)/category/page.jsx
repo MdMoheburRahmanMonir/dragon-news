bf49d1a9c9aca1861a178da4dd86bddf4a929fad
mdmoheburrahmanmonir <mdmohiburrahmanmanik@gmail.com>
2026-04-28 14:48:48 +0600

    Added something

--- src/app/(main)/category/page.jsx
+++ src/app/(main)/category/page.jsx
@@ -1,1 +1,12 @@
+ import React from 'react';
  
+ const page = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-gray-700 font-bold text-start mb-4'>Dragon News Category</h1>
+             <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        </div>
+    );
+ };
+ 
+ export default page;
